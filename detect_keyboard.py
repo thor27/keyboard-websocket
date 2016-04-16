24c6cd6dff316b7ee6a49629c33a5eba99a24cff
Thomaz Reis <thor27@gmail.com>
2016-04-16 18:13:55 -0300

    Code cleanup & refactor & pep8 & pyflakes & optional argument for keyboard file

--- detect_keyboard.py
+++ detect_keyboard.py
@@ -1,34 +1,53 @@
  #-*- coding: utf-8 -*-
 
 import evdev
+import sys
+import argparse
 from time import sleep
+
 devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
 open_devices = [evdev.InputDevice(device.fn) for device in devices]
 
-print 'Please wait... Do NOT press any key right now, cleaning up inputs...'
-sleep(1)
-for num,device in enumerate(open_devices):
-    max_count = 5
-    while device.read_one():
-        max_count -= 1
-        if not max_count:
-            print 'Device', open_devices[num], 'seems too busy, removing!'
-            del open_devices[num]
-            break
 
-print 'Thanks.'
-print 'Now, press any key on the correct keyboard'
+def clean_up():
+    print 'Please wait... Do NOT press any key right now, cleaning up inputs...'
+    sleep(1)
+    for num, device in enumerate(open_devices):
+        max_count = 5
+        while device.read_one():
+            max_count -= 1
+            if not max_count:
+                print 'Device', open_devices[num], 'seems too busy, removing!'
+                del open_devices[num]
+                break
+    print 'Thanks.'
 
-correct_device = None
-while True:
-    for num,device in enumerate(open_devices):
-        if device.read_one():
-            correct_device = device
-            break
-    if correct_device:
-        break
 
-with open('keyboard.cfg', 'w') as config:
-    config.write(device.fn + '\n')
+def detect_key():
+    print 'Now, press any key on the correct keyboard'
+    while True:
+        for num, device in enumerate(open_devices):
+            if device.read_one():
+                return device
 
-print 'Done, correct device is:', device
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description='Detect keyboard device.')
+    parser.add_argument('-o', '--file-output', dest='outfile', type=str, default='keyboard.cfg', help='Output file (Default: keyboard.cfg)')
+    return parser.parse_args(args)
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+
+    clean_up()
+    device = detect_key()
+
+    with open(args.outfile, 'w') as outfile:
+        outfile.write(device.fn + '\n')
+
+    print 'Done, correct device is:', device
+
+
+if __name__ == '__main__':
+    main()
