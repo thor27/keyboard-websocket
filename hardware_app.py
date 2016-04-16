24f6df4ba36625be4ff17de6ec776529a8848c9a
Thomaz Reis <thor27@gmail.com>
2016-04-16 19:01:25 -0300

    Hardware_app refactoring & cleanup & pep8 & pyflakes

--- hardware_app.py
+++ hardware_app.py
@@ -2,10 +2,13 @@
 
 import evdev
 import xinput
-
+import argparse
+import sys
+import os
 from socketIO_client import SocketIO
 
-def read_keyboard(device, ws):
+
+def read_keyboard(device, ws, silent=False):
     for event in device.read_loop():
         if event.type == evdev.ecodes.EV_KEY:
             key_event = evdev.categorize(event)
@@ -14,12 +17,15 @@ def read_keyboard(device, ws):
                 'type': get_type(key_event),
                 'data': key_event.keycode
             })
-            print get_type(key_event), key_event.keycode
+            if not silent:
+                print(get_type(key_event), key_event.keycode)
 
-def load_device_filename():
-    with open('keyboard.cfg') as config:
-        filename = config.read().replace('\n','')
+
+def load_device_filename(keyboard_config_file):
+    with open(keyboard_config_file) as config:
+        filename = config.read().replace('\n', '')
     return filename
+
 
 def get_type(key_event):
     if key_event.keystate == key_event.key_down:
@@ -29,8 +35,35 @@ def get_type(key_event):
     if key_event.keystate == key_event.key_hold:
         return 'HOLD'
 
-with SocketIO('localhost', 5000) as ws:
-    device_filename = load_device_filename()
+
+def parse_args(args):
+    keyboardcfg = os.path.join(os.path.dirname(__file__), 'keyboard.cfg')
+    parser = argparse.ArgumentParser(description='Send keyboard input to SocketIO')
+    parser.add_argument('-f', '--config-file', dest='keyboard_config_file', type=str, default=keyboardcfg,
+                        help='Keyboard Config File (Default: keyboard.cfg)')
+    parser.add_argument('-d', '--device', dest='device_filename', type=str, default='',
+                        help='Specify device to read. (Default: Read from Keyboard Config File)')
+    parser.add_argument('-H', '--hostname', dest='hostname', type=str, default='localhost',
+                        help='Specify socketIO hostname to connect. (Default: localhost)')
+    parser.add_argument('-p', '--port', dest='port', type=int, default=5000,
+                        help='Specify socketIO port to connect. (Default: 5000)')
+    parser.add_argument('-s', '--silent', dest='silent', action='store_true', help='Don\'t print to stdout')
+
+    return parser.parse_args(args)
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+    if args.device_filename:
+        device_filename = args.device_filename
+    else:
+        device_filename = load_device_filename(args.keyboard_config_file)
+
     device = evdev.InputDevice(device_filename)
     xinput.disable_device(device_filename)
-    read_keyboard(device, ws)
+    with SocketIO(args.hostname, args.port) as ws:
+        read_keyboard(device, ws, args.silent)
+
+
+if __name__ == '__main__':
+    main()
