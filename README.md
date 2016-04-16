c3da6f75d47a357e74c43533f690a85339c038c4
Thomaz Reis <thor27@gmail.com>
2016-04-16 01:30:53 -0300

    Alterado para rodar junto com o gwmanager

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 Read evdev device and send to websocket
 
 * **hardware_app.py**: Read device from evdev and send key events to websocket.
-* **app.py**: Websocket server, receive websocket message from hardware and broadcast it.
 * **detect_keyboard.py**: Detect which keyboard to use, and saves in keyboard.cfg
 
 1. Run **detect_keyboard.py** as root (or evdev allowed user) once to configure.
-2. Run **app.py**
+2. Run **gwmanager start --port=8000**
 3. Run **hardware_app.py** as root (or evdev allowed user)
-4. open <http://localhost:5000/>
+4. open <http://localhost:8000/>
--- hardware_app.py
+++ hardware_app.py
@@ -2,24 +2,29 @@
 
 import evdev
 import xinput
+import json
+import sys
+from time import sleep
+from websocket import create_connection, socket
 
-from socketIO_client import SocketIO
 
 def read_keyboard(device, ws):
     for event in device.read_loop():
         if event.type == evdev.ecodes.EV_KEY:
             key_event = evdev.categorize(event)
-
-            ws.emit('hardware_event', {
+            event_data = {
                 'type': get_type(key_event),
-                'data': key_event.keycode
-            })
-            print get_type(key_event), key_event.keycode
+                'keycode': key_event.keycode
+            }
+            ws.send(json.dumps(event_data))
+            print(event_data)
+
 
 def load_device_filename():
     with open('keyboard.cfg') as config:
         filename = config.read().replace('\n','')
     return filename
+
 
 def get_type(key_event):
     if key_event.keystate == key_event.key_down:
@@ -29,8 +34,21 @@ def get_type(key_event):
     if key_event.keystate == key_event.key_hold:
         return 'HOLD'
 
-with SocketIO('localhost', 5000) as ws:
-    device_filename = load_device_filename()
-    device = evdev.InputDevice(device_filename)
-    xinput.disable_device(device_filename)
-    read_keyboard(device, ws)
+
+def main(port):
+    url = "ws://localhost:{0}/broadcast/websocket".format(port)
+    ws = None
+    try:
+        ws = create_connection(url)
+        device_filename = load_device_filename()
+        device = evdev.InputDevice(device_filename)
+        xinput.disable_device(device_filename)
+        read_keyboard(device, ws)
+    finally:
+        if ws:
+            ws.close()
+
+
+if __name__ == '__main__':
+    port = sys.argv[1] if len(sys.argv) > 1 else '8000'
+    main(port)
