ea8d75b22ac7fbbe51ea208e82de6e12cc844c1a
Thomaz Reis <thor27@gmail.com>
2016-04-16 19:37:04 -0300

    App refactoring & cleanup & pep8 & pyflakes

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import argparse
+import sys
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
@@ -12,13 +13,31 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
-@socketio.on('connect')
-def test_connect():
-    emit('ok')
 
 @socketio.on('hardware_event')
 def test_message(message):
     emit('key_event', message, broadcast=True)
 
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description='Keyboard Websocket webserver and Testground')
+    parser.add_argument('-H', '--hostname', dest='hostname', type=str, default='localhost',
+                        help='Specify socketIO hostname to connect. (Default: localhost)')
+    parser.add_argument('-p', '--port', dest='port', type=int, default=5000,
+                        help='Specify socketIO port to connect. (Default: 5000)')
+    parser.add_argument('-s', '--silent', dest='silent', action='store_true', help='Don\'t print to stdout')
+
+    return parser.parse_args(args)
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+    debug = not args.silent
+
+    if debug:
+        print 'Listening on http://{0}:{1}/socket.io'.format(args.hostname, args.port)
+
+    socketio.run(app, host=args.hostname, port=args.port, debug=debug)
+
 if __name__ == '__main__':
-    socketio.run(app)
+    main()
