52962df5ff7bdea042ef74c3c475d2b76f861815
Thomaz Reis <thor27@gmail.com>
2016-04-27 00:06:33 -0300

    Melhorado script de stop_gunicorn

--- stop_gunicorn.sh
+++ stop_gunicorn.sh
@@ -3,14 +3,14 @@ cd "`dirname $0`"
 if [ -f logs/gunicorn.pid ]
 then
   PID=`cat logs/gunicorn.pid`
-  ps $PID >/dev/null && kill $PID >/dev/null
-  sleep 1
   if ps $PID >/dev/null
   then
     kill $PID >/dev/null
-    sleep 1
+    sleep 0.5
     if ps $PID >/dev/null
     then
+      kill $PID >/dev/null
+      sleep 1
       ps $PID >/dev/null && kill -9 $PID >/dev/null
     fi
   fi
